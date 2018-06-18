3faca89ace571d984135a859b720037443fc0304
nickymikail <hasselln@gmail.com>
2018-06-18 16:49:31 -0700

    Add load job functionality

--- target_bigquery.py
+++ target_bigquery.py
@@ -15,10 +15,13 @@ from jsonschema import validate
 import singer
 
 from oauth2client import tools
+from tempfile import TemporaryFile
 
 from google.cloud import bigquery
+from google.cloud.bigquery.job import SourceFormat
 from google.cloud.bigquery import Dataset
 from google.cloud.bigquery import SchemaField
+from google.cloud.bigquery import LoadJobConfig
 from google.api_core import exceptions
 
 try:
@@ -44,6 +47,9 @@ def emit_state(state):
         logger.debug('Emitting state {}'.format(line))
         sys.stdout.write("{}\n".format(line))
         sys.stdout.flush()
+
+def clear_dict_hook(items):
+    return {k: v if v is not None else '' for k, v in items}
 
 def define_schema(field, name):
     schema_name = name
@@ -95,7 +101,86 @@ def build_schema(schema):
 
     return SCHEMA
 
-def persist_lines(project_id, dataset_id, lines=None):
+def persist_lines_job(project_id, dataset_id, lines=None):
+    state = None
+    schemas = {}
+    key_properties = {}
+    tables = {}
+    rows = {}
+    errors = {}
+
+    bigquery_client = bigquery.Client(project=project_id)
+
+    # try:
+    #     dataset = bigquery_client.create_dataset(Dataset(dataset_ref)) or Dataset(dataset_ref)
+    # except exceptions.Conflict:
+    #     pass
+
+    for line in lines:
+        try:
+            msg = singer.parse_message(line)
+        except json.decoder.JSONDecodeError:
+            logger.error("Unable to parse:\n{}".format(line))
+            raise
+
+        if isinstance(msg, singer.RecordMessage):
+            if msg.stream not in schemas:
+                raise Exception("A record for stream {} was encountered before a corresponding schema".format(msg.stream))
+
+            schema = schemas[msg.stream]
+
+            validate(msg.record, schema)
+
+            dat = bytes(str(json.loads(json.dumps(msg.record), object_pairs_hook=clear_dict_hook)) + '\n', 'UTF-8')
+            
+            rows[msg.stream].write(dat)
+            #rows[msg.stream].write(bytes(str(msg.record) + '\n', 'UTF-8'))
+
+            state = None
+
+        elif isinstance(msg, singer.StateMessage):
+            logger.debug('Setting state to {}'.format(msg.value))
+            state = msg.value
+
+        elif isinstance(msg, singer.SchemaMessage):
+            table = msg.stream 
+            schemas[table] = msg.schema
+            key_properties[table] = msg.key_properties
+            #tables[table] = bigquery.Table(dataset.table(table), schema=build_schema(schemas[table]))
+            rows[table] = TemporaryFile(mode='w+b')
+            errors[table] = None
+            # try:
+            #     tables[table] = bigquery_client.create_table(tables[table])
+            # except exceptions.Conflict:
+            #     pass
+
+        elif isinstance(msg, singer.ActivateVersionMessage):
+            # This is experimental and won't be used yet
+            pass
+
+        else:
+            raise Exception("Unrecognized message {}".format(msg))
+
+    for table in rows.keys():
+        table_ref = bigquery_client.dataset(dataset_id).table(table)
+        SCHEMA = build_schema(schemas[table])
+        load_config = LoadJobConfig()
+        load_config.schema = SCHEMA
+        load_config.source_format = SourceFormat.NEWLINE_DELIMITED_JSON
+        rows[table].seek(0)
+        bigquery_client.load_table_from_file(
+            rows[table], table_ref, job_config=load_config)
+
+
+    # for table in errors.keys():
+    #     if not errors[table]:
+    #         print('Loaded {} row(s) into {}:{}'.format(rows[table], dataset_id, table), tables[table].path)
+    #     else:
+    #         print('Errors:', errors[table], sep=" ")
+
+    return state
+
+def persist_lines_stream(project_id, dataset_id, lines=None):
     state = None
     schemas = {}
     key_properties = {}
@@ -176,7 +261,7 @@ def collect():
             'se_la': version,
         }
         conn.request('GET', '/i?' + urllib.parse.urlencode(params))
-        response = conn.getresponse()
+        conn.getresponse()
         conn.close()
     except:
         logger.debug('Collection request failed')
@@ -193,7 +278,7 @@ def main():
 
     input = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
 
-    state = persist_lines(config['project_id'], config['dataset_id'], input)
+    state = persist_lines_stream(config['project_id'], config['dataset_id'], input) if config.get('stream_data', True) else persist_lines_job(config['project_id'], config['dataset_id'], input)
     emit_state(state)
     logger.debug("Exiting normally")
 
